13c4811654ac8d5bafcd8f9488b13341efd92f48
hamzaakrammogul <hmughal502@gmail.com>
2023-06-08 21:14:20 +0500

    Forms and Inputs

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,49 +1,87 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, {  useState } from "react";
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState('');
-  const [isValid, setIsValid] = useState(false);
-  const [isTouched, setIsTouched] = useState(false)
-  const useRefInput = useRef('');
 
-  useEffect(()=>{
-    if (isValid
-      ){
-        console.log('Name inout is Valid');
-      }
-  }, [isValid]);
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredEmail, setEnteredEmail]=  useState('');
+  // const [isValid, setIsValid] = useState(false);
+  const [isTouched, setIsTouched] = useState(false);
+  //const [formIsValid, setFormIsValid] = useState(false);
+
+
+  const isValid = enteredName.trim() !== ''; 
+  const inputIsInvalid = !isValid && isTouched;
+
+  const emailIsValid = enteredEmail.trim() !== '' && enteredEmail.includes('@');
+  const emailInputIsValid = enteredEmail.trim() !== '' && enteredEmail.includes('@') && isTouched;
+
+  // useEffect(()=>{
+  //   if(isValid){
+  //     setFormIsValid(true);
+  //   }else {
+  //     setFormIsValid(false);
+  //   }
+  // }, [isValid]);
+
+  let formIsValid = false;
+
+  if(isValid){
+    formIsValid=true;
+  };
 
   const onChangeInputHandler = (event) => {
     setEnteredName(event.target.value);
+    // if (event.target.value.trim() !== '') {
+    //   setIsValid(true);
+    // }
+  };
+
+  const onBlurChangeHandler = event => {
+    setIsTouched(true);
+    // if (enteredName.trim() === '') {
+    //   setIsValid(false);
+    // };
+  };
+
+  const onEmailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
   }
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
     setIsTouched(true);
-    if (enteredName.trim() === '') {
-      setIsValid(false);
+    // if (enteredName.trim() === '') {
+    //   setIsValid(false);
+    //   return;
+    // }
+    // setIsValid(true)
+    if(!isValid && !emailIsValid){
       return;
     }
-    setIsValid(true)
     console.log(enteredName);
-  }
+    setEnteredName('');
+    setIsTouched(false);
+  };
 
-  const inputIsInvalid = !isValid && isTouched ;
   
-  // const enteredValue = useRefInput.current.value;
-  // console.log("Use Ref " + enteredValue);
-
   const nameInpuClasses = inputIsInvalid ? 'form-control invalid' : 'form-control';
+
+  const emailInpuClasses = emailInputIsValid ? 'form-control invalid' : 'form-control';
+
 
   return (
     <form onSubmit={onSubmitHandler}>
       <div className={nameInpuClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={useRefInput} type='text' id='name' onChange={onChangeInputHandler} />
+        <input value={enteredName} type='text' id='name' onChange={onChangeInputHandler} onBlur={onBlurChangeHandler} />
         {inputIsInvalid && <p className="error-text">Name must not be empty!</p>}
+        </div>
+        <div className={emailInpuClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input type="text" id="email" onChange={onEmailChangeHandler}></input>
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
