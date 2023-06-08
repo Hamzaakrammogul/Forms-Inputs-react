db44a64821bdbeaa8fd21411e0fa5697bf6b7a36
hamzaakrammogul <hmughal502@gmail.com>
2023-06-08 17:02:37 +0500

    Validating after submit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,46 @@
+import React, { useState, useRef, useEffect } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [isTouched, setIsTouched] = useState(false)
+  const useRefInput = useRef('');
+
+  useEffect(()=>{
+    if (isValid
+      ){
+        console.log('Name inout is Valid');
+      }
+  }, [isValid]);
+
+  const onChangeInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    setIsTouched(true);
+    if (enteredName.trim() === '') {
+      setIsValid(false);
+      return;
+    }
+    setIsValid(true)
+    console.log(enteredName);
+  }
+
+  const inputIsInvalid = !isValid && isTouched ;
+  
+  // const enteredValue = useRefInput.current.value;
+  // console.log("Use Ref " + enteredValue);
+
+  const nameInpuClasses = inputIsInvalid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={onSubmitHandler}>
+      <div className={nameInpuClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={useRefInput} type='text' id='name' onChange={onChangeInputHandler} />
+        {inputIsInvalid && <p className="error-text">Name must not be empty!</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
